cfc5ad4c7efe19ac4543f0b71ee3628ee83297a5
kambizzandi <kambizzandi@gmail.com>
2021-11-27 11:43:31 +0330

    add command for copying swaggerui via docker

--- Docker/export-dev.sh
+++ Docker/export-dev.sh
@@ -9,6 +9,8 @@ echo copying library
 docker cp ../out/lib64 activeAPI:/targoman/.
 echo copying config files
 docker cp ../conf/TextProcessor/. activeAPI:/targoman/conf/TextProcessor/.
+echo copying swaggerui files
+docker cp ../SwaggerUI activeAPI:/targoman/.
 
 echo Restarting 
 #sudo docker restart activeAPI
