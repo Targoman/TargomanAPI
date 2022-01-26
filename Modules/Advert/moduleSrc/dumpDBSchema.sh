fbe1b99103a3d37c2d8c0422cd157e6284ca3641
kambizzandi <kambizzandi@gmail.com>
2022-01-26 12:22:31 +0300

    .migrations added to .gitignore

--- Modules/Advert/moduleSrc/dumpDBSchema.sh
+++ Modules/Advert/moduleSrc/dumpDBSchema.sh
@@ -4,7 +4,12 @@ mysqldump \
     --tables --routines --triggers --events \
     --no-data --skip-comments --add-drop-trigger \
     --protocol=tcp --user=root --password=targoman123 \
+<<<<<<< Updated upstream
     -B Advert \
     | sed 's/ AUTO_INCREMENT=[0-9]*//g' \
     | sed 's/ DEFINER=`root`@`%`//g' \
     > ORM/Schema.my.sql
+=======
+    Advert \
+    | sed 's/ AUTO_INCREMENT=[0-9]*//g' > ORM/Schema.my.sql
+>>>>>>> Stashed changes
