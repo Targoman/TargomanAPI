1679213c6c67c54800c1188a9e0268d65a5816c8
kambizzandi <kambizzandi@gmail.com>
2022-05-25 18:13:11 +0430

    set migration file as ready to commit

--- Modules/Account/migrations/db/m20220525_170624_AAA_dbdiffof_dev_AAA.sql
+++ Modules/Account/migrations/db/m20220525_170624_AAA_dbdiffof_dev_AAA.sql
@@ -1,9 +1,6 @@
 /* Migration File: m20220525_170624_AAA_dbdiffof_dev_AAA.sql */
 /* CAUTION: don't forget to use {{dbprefix}} for schemas */
 
-/* The next line is to prevent this file from being committed. When done, delete this and next line: */
-ERROR("THIS MIGRATION FILE IS NOT READY FOR EXECUTE.")
-
 USE `{{dbprefix}}{{Schema}}`;
 
 /************************************************************\
