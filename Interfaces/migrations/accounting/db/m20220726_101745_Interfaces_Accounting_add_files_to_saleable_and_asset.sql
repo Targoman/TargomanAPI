e186131f328d92de375f450f84e3cddaeb3f8760
kambizzandi <kambizzandi@gmail.com>
2022-07-26 13:26:03 +0430

    working on it

---
+++ Interfaces/migrations/accounting/db/m20220726_101745_Interfaces_Accounting_add_files_to_saleable_and_asset.sql
@@ -0,0 +1,8 @@
+/* Migration File: m20220726_101745_Interfaces_Accounting_add_files_to_saleable_and_asset.sql */
+/* CAUTION: don't forget to use {{dbprefix}} for schemas */
+
+/* The next line is to prevent this file from being committed. When done, delete this and next line: */
+ERROR("THIS MIGRATION FILE IS NOT READY FOR EXECUTE.")
+
+USE `{{dbprefix}}{{Schema}}`;
+
